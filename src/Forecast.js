e4a0a293aca43fc127be490f7d41550fc7b9cda7
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2025-07-15 20:53:19 +0100

    Added iframe unique id and removed unused vars

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Forecast.css";
 
 export default function Forecast() {
--- src/Header-searchbutton.js
+++ src/Header-searchbutton.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Forecast from "./Forecast";
 
 export default function SearchEngine() {
   const [city, setCity] = useState("");
