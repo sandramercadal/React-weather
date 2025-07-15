ad907301d507b1b39eb96b597ed71ea7ce7670a0
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2025-07-15 15:47:00 +0100

    Added city and temp on load hardcoded

--- src/CurrentDetails.js
+++ src/CurrentDetails.js
@@ -9,10 +9,16 @@ export default function CurrentDetails() {
           <span> Last updated:Monday 07:33</span>
         </em>
       </h3>
-      <h2> Madrid</h2>
-      <h1> ES</h1>
+      <h2>Madrid</h2>
+      <h1>ES</h1>
+      <h1>Clear Sky</h1>
+      <img
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+        alt="forecast_icon"
+        width="62"
+      />
       <h4>30 degrees</h4>
-      <h4>Weather icon and stats for Madrid</h4>
+      <h4>Humidity: 19% / Wind: 4km/h</h4>
     </div>
   );
 }
--- src/Header-searchbutton.js
+++ src/Header-searchbutton.js
@@ -66,4 +66,3 @@ export default function SearchEngine() {
     return form;
   }
 }
-<hr />;
